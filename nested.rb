b683ceeac2941e3f20bf860e285fba15796f61bb
ba hanlon <herbivore137@gmail.com>
2019-04-14 19:17:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,6 +67,10 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
 
 	programmer_hash = 
  		{
@@ -85,10 +89,7 @@ def adding_matz
      }
      
      
-  programmer_hash[:yukihiro_matsumoto] = {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  }
+  
     
 end
 
